c85da3feb7269a03ca2261b39ee45c0cca952b20
masa-k0101 <65117419+masa-k0101@users.noreply.github.com>
2020-05-29 23:03:31 +0900

    Add files via upload

---
+++ Cp4/4-2-1_trainning.cpp
@@ -0,0 +1,34 @@
+// オブジェクトへのポインタ
+#include<iostream>
+
+namespace A
+{
+    class samp
+    {
+            int a, b;
+        public:
+            samp(int n, int m)   { a = n; b = m; }
+            int get_a()   { return a; }
+            int get_b()   { return b; }
+    };
+}
+
+main()
+{
+    A::samp ob[4] =
+    {
+        A::samp(1,2), A::samp(3,4), A::samp(5,6), A::samp(7,8)
+    };
+    int i;
+
+    A::samp *p;
+    p = ob;     //配列の先頭アドレスを得る
+
+    for(i=0; i<4; i++)
+        {   std::cout << p->get_a() << ' ';
+            std::cout << p->get_b() << "\n";
+            p++;    //次のオブジェクトへ進む
+        }
+    std::cout << "\n";
+    return 0;
+}
