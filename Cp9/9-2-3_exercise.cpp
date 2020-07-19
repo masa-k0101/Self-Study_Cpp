5e9fc53ba1ca33aa37a5f9b24b621bc0ab9ba7fe
masa-k0101 <65117419+masa-k0101@users.noreply.github.com>
2020-07-20 00:17:27 +0900

    Add files via upload

---
+++ Cp9/9-2-3_exercise.cpp
@@ -0,0 +1,40 @@
+// Word count
+#include<iostream>
+#include<fstream>
+#include<ctype.h>
+
+main(int argc, char *argv[])
+{
+    if(argc!=2)
+    {
+        std::cout << "Usage: COUNT <input>\n";
+        return 1;
+    }
+
+    std::ifstream in(argv[1]);     // 入力ファイルをオープン
+
+    if(!in)
+    {
+        std::cout << "Cannot open input file\n";
+        return 1;
+    }
+
+    int count = 0;
+    char ch;
+
+    in >> ch;       // find first nos-space char
+
+    // after first non-space found
+    in.unsetf(std::ios::skipws);        // スペースをスキップしない
+    while(!in.eof())
+    {
+        in >> ch;
+        if(isspace(ch)) count++; 
+    }    
+
+    std::cout << "Word count: " << count << '\n';
+    
+    in.close();
+
+    return 0;
+}
