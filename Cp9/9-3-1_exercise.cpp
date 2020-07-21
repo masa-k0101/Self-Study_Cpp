0ca876844a52cb0c0c4dc81157ece8c76b7c9943
masa-k0101 <65117419+masa-k0101@users.noreply.github.com>
2020-07-22 07:46:27 +0900

    Add files via upload

---
+++ Cp9/9-3-1_exercise.cpp
@@ -0,0 +1,40 @@
+// Copy a file and display number of chars copied
+#include<iostream>
+#include<fstream>
+
+main(int argc, char *argv[])
+{
+    if(argc!=3)
+    {
+        std::cout << "Usage: COPY <input> <output>\n";
+        return 1;
+    }
+
+    std::ifstream fin(argv[1]);     // 入力ファイルをオープン
+    std::ofstream fout(argv[2]);    // 出力ファイルを作る
+
+    if(!fout)
+    {
+        std::cout << "Cannot open output file.\n";
+        return 1;
+    }
+
+    if(!fin)
+    {
+        std::cout << "Cannot open input file.\n";
+        return 1;
+    }
+
+    char ch;
+    unsigned count = 0;
+    
+    while(!fin.eof())
+    {
+        fin.get(ch);
+        fout.put(ch);
+        count++;
+    }    
+
+    std::cout << "Number of bytes copied: " << count << '\n';
+    return 0;
+}
