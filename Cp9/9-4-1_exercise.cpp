1d7cbb04e47c6f1043515fc39e8f7c72c90d67a2
masa-k0101 <65117419+masa-k0101@users.noreply.github.com>
2020-07-30 21:00:59 +0900

    Add files via upload

---
+++ Cp9/9-4-1_exercise.cpp
@@ -0,0 +1,15 @@
+// getline()を使ってスペースを含む文字列を読む
+#include<iostream>
+#include<fstream>
+
+main()
+{
+    char str[80];
+
+    std::cout << "Enter your name: ";
+    std::cin.get(str, 79);
+
+    std::cout << str << '\n';
+
+    return 0;
+}
