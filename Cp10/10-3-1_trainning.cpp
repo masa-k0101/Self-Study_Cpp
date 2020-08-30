244adbd7b89f51a1c172b1742023fdf1cccb2724
masa-k0101 <65117419+masa-k0101@users.noreply.github.com>
2020-08-31 00:41:27 +0900

    Add files via upload

---
+++ Cp10/10-3-1_trainning.cpp
@@ -0,0 +1,61 @@
+// 抽象クラスを作る
+#include<iostream>
+
+class area
+{
+        double dim1, dim2;  // 図形の寸法
+    public:
+        void setarea(double d1, double d2)
+        {
+            dim1 = d1;
+            dim2 = d2;
+        }
+        void getdim(double &d1, double &d2)
+        {
+            d1 = dim1;
+            d2 = dim2;
+        }
+        virtual double getarea() = 0;   // 純粋仮想関数
+};
+
+class rectangle : public area
+{
+    public:
+        double getarea()
+        {
+            double d1, d2;
+
+            getdim(d1, d2);
+            return d1 * d2;
+        }
+};
+
+class triangle : public area
+{
+    public:
+        double getarea()
+        {
+            double d1, d2;
+
+            getdim(d1, d2);
+            return 0.5 * d1 * d2;
+        }
+};
+
+main()
+{
+    area *p;
+    rectangle r;
+    triangle t;
+
+    r.setarea(3.3, 4.5);
+    t.setarea(4.0, 5.0);
+
+    p = &r;
+    std::cout << "Rectangle has area: " << p->getarea() << '\n';
+
+    p = &t;
+    std::cout << "Triangle has area: " << p->getarea() << '\n';
+
+    return 0;
+}
